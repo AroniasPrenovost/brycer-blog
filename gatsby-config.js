1e51ae9a66fc2b8bc56e4a26bf3f13d28edfdb26
Aron Jon Prenovost <Aroniascoder@gmail.com>
2020-01-12 14:59:15 -0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `Case of the Mondays`,
+    author: `Bryce Prenovost`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
